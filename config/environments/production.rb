c24c4d93d5d526c9179de4c60f0d61e39cbedcb4
laurant94 <namedemathieu@gmail.com>
2021-07-24 10:26:36 +0100

    change_production_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "https://morning-coast-91579.herokuapp.com/", port: 80 }
+  config.action_mailer.default_url_options = { host: "https://morning-coast-91579.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
